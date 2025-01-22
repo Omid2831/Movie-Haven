1b939cd6586157919884e8a762c62e5cbfdbeddf
unknown <341381@studet.mboutrecht.nl>
2025-01-22 12:18:45 +0100

    added popup cards when clicking image

--- src/SRC/src/Javascript/showpopup.js
+++ src/SRC/src/Javascript/showpopup.js
@@ -1,3 +1,20 @@
-function showItem() {
-    console.log("hi");
+const popup = document.getElementById("popUp");
+
+function showItem(item) {
+    const movie = document.getElementsByClassName(item);
+    for (let i = 0; i < movie.length; i++) {
+        movie[i].style.display = "initial";
+    }
+    popup.style.display = "initial";
+
+
+}
+
+function closepopUp() {
+    const movieinfo = document.getElementsByClassName("popUpInfo")
+    popup.style.display = "";
+    for (let i = 0; i < movieinfo.length; i++) {
+        movieinfo[i].style.display = "";
+    }
+    
 }
